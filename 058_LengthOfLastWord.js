5ab848471a67b8821730229f51ed16a929d7d99b
lon <dove2580@live.com>
2017-05-02 20:00:29 +0800

    Improve Length Of Last Word

--- 058_LengthOfLastWord.js
+++ 058_LengthOfLastWord.js
@@ -4,11 +4,31 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function (s) {
+// Simple
+var lengthOfLastWord2 = function (s) {
   if (!s) return 0;
   s = s.trimRight();
   var arr = s.split(' ');
   return arr[arr.length - 1].length;
+};
+
+
+// Improve
+var lengthOfLastWord = function (s) {
+  if (!s) return 0;
+  var started = false;
+  var length = 0;
+  for (var i = s.length - 1; i >= 0; i--) {
+    if (s[i] !== ' ' && !started) {
+      started = true;
+    }
+    if (!started) continue;
+    if (started && s[i] === ' ') {
+      break;
+    }
+    length++;
+  }
+  return length;
 };
 
 var s = "Hello World";
@@ -24,4 +44,7 @@ s = "  aaa";
 console.log(lengthOfLastWord(s));
 
 s = "a";
+console.log(lengthOfLastWord(s));
+
+s = " ";
 console.log(lengthOfLastWord(s));
