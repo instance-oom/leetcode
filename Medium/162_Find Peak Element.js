67ebece97cd2ea46fc936e558218d51f44f9e1c8
lon <dove2580@live.com>
2018-03-30 19:54:35 +0800

    Add Find Pead Element

---
+++ Medium/162_Find Peak Element.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/find-peak-element/description/#
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+};
+
+console.log(findPeakElement([1, 2, 3, 1]));
